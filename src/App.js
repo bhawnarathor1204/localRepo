330cef6f2f32d4203052273f394d7315ef2cce20
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-10-08 20:05:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>This is my App1</h1>
+       <h1>This is my App2</h1>
+       <h1>This is my App3</h1>
+       <h1>This is my App4</h1>
+    </div>
+  );
+}
+
+export default App;
